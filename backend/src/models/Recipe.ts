75257253d62f25a74ebe1198d4d9fbc804be2096
juanss3 <juanmanuelss3@gmail.com>
2024-12-05 04:02:19 -0500

    conexion con la base de adtos, config del server y app y modelo del recipe

---
+++ backend/src/models/Recipe.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IRecipe extends Document {
+    title: string;
+    category: string;
+    ingredients: string[];
+    instructions: string;
+    image?: string;
+}
+
+const RecipeSchema: Schema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: [String],
+        required: true,
+    },
+    instructions: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+});
+
+export default mongoose.model<IRecipe>("Recipe", RecipeSchema);
